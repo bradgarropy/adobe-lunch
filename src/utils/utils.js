75e4aea9070cd76f579cba3b0d7c5c25abe3bb3d
Brad Garropy <bradgarropy@gmail.com>
2019-10-10 23:55:58 -0500

    no console logs.

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,9 +1,6 @@
 const getRandomElement = array => {
-    console.log(array)
     const index = Math.floor(Math.random() * array.length)
-    console.log(index)
     const element = array[index]
-    console.log(element)
 
     return element
 }
