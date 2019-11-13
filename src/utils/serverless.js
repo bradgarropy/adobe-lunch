fa330ef1e8deb14217f3b6736742e448f501fc1d
Brad Garropy <bradgarropy@gmail.com>
2019-11-12 21:45:49 -0600

    google serverless functions.

--- src/utils/serverless.js
+++ src/utils/serverless.js
@@ -20,6 +20,28 @@ const details = async id => {
     return json
 }
 
+const googlesearch = async() => {
+    const response = await fetch("/.netlify/functions/googlesearch")
+    const json = await response.json()
+
+    return json
+}
+
+const googledetails = async id => {
+    const data = {id}
+
+    const options = {
+        "method": "POST",
+        "Content-Type": "application/json",
+        "body": JSON.stringify(data),
+    }
+
+    const response = await fetch("/.netlify/functions/googledetails", options)
+    const json = await response.json()
+
+    return json
+}
+
 const accept = async id => {
     const data = {id}
 
@@ -46,4 +68,11 @@ const reject = async id => {
     return
 }
 
-export default {accept, reject, search, details}
+export default {
+    accept,
+    reject,
+    search,
+    details,
+    googlesearch,
+    googledetails,
+}
